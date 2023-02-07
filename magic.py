91a767e2efdf9828aad1af0a7018a5ec6fff944a
Junior <joseinadilsonjunior@gmail.com>
2023-02-07 11:42:59 -0300

    primiero commit

---
+++ magic.py
@@ -0,0 +1,13 @@
+import pygame
+from settings import *
+
+
+class MagicPlayer:
+    def __init__(self, animation_player):
+        self.animation_player = animation_player
+
+    def heal(self):
+        print('heal')
+
+    def flame(self):
+        pass
